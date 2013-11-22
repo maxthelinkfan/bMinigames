d3d83e1564fe064b117cb5aa07d5804ea20c4909
maxthelinkfan <maxlikeabaws@gmail.com>
2013-11-21 19:36:16 -0600

    Basics

---
+++ src/me/brawl/Main/Main.java
@@ -0,0 +1,15 @@
+package me.brawl.Main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public void onEnable() {
+	
+	}
+
+	public void onDisable() {
+		
+	}
+
+}
