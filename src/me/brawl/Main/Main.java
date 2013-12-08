98c8873d9cd0c955d07444788532708765a66bd6
Hydrations <maxholzmann@gmail.com>
2013-12-08 16:35:11 -0600

    Adds a "launchPlayer" method

--- src/me/brawl/Main/Main.java
+++ src/me/brawl/Main/Main.java
@@ -1,8 +1,17 @@
 package me.brawl.Main;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 public class Main extends JavaPlugin {
+	
+	public void launchPlayer(Player player) {
+		Vector vector = player.getEyeLocation().getDirection();
+		vector.multiply(1.0F);
+		vector.setY(2.0);
+		player.setVelocity(vector);
+	}
 	
 	public void onEnable() {
 	
